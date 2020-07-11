4d4ea0ac94ed30ac9976690b0a8599be8f30f63b
Brody Dingel <brodydingel795@gmail.com>
2020-07-10 20:31:47 -0500

    Add run script

--- server/routes/index.js
+++ server/routes/index.js
@@ -34,61 +34,6 @@ router.get('/sitemap', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/../sitemap.xml'));
 });
 
-// router.get('/', function(req, res, next) {
-//   res.sendFile(path.join(__dirname+'/../brody/index.html'));
-// });
-
-router.post('/', urlencodedParser, function(req, res, next) {
-  var contacterData = req.body;
-  var contacterName = contacterData.contacterName;
-  var contacterEmail = contacterData.contacterEmail;
-  var contacterMessage = contacterData.contacterMessage;
-  //fs.appendFile("debugging.txt", JSON.stringify(req.body), (err)=>{if (err) throw err;});
-
-  if (!req.body.captcha){
-    return res.json({msg: 'captcha token is undefined'});
-  }
-
-  const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}`;
-
-  request(verifyUrl,(err, response, body)=>{
-    if (err) {
-      console.log(err);
-    }
-
-    body = JSON.parse(body);
-
-    if (!body.success || body.score < 0.4) {
-      return res.json({'msg':'you might be a robot', 'score': body.score});
-    }
-    var email_text = contacterName + ' sent you a message on your personal website. You can contact them at ' + contacterEmail + '. \n\nMessage:\n' + contacterMessage;
-
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASS
-      }
-    });
-
-    var mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: process.env.EMAIL_RECIPIENT_PERSONAL_SITE,
-      subject: "Personal message from " + contacterName,
-      text: email_text
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        pass
-      } else {
-        pass
-      }
-    });
-    return res.json({'msg':'you have been verified', 'score': body.score, 'contacterData': contacterData});
-  });
-});
-
 
 router.get('/index-old', function (req, res, next) {
   res.render('index', { title: 'Overview' });
